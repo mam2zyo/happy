fed1069ce9700e211e0f9e7dd9d008d631809e00
Park Shinhee <mam2zyo@gmail.com>
2026-09-09 23:58:30 +0900

    update coordinate

--- public/js/config.js
+++ public/js/config.js
@@ -11,9 +11,9 @@ window.INVITATION_CONFIG = {
   TMAP_APP_KEY: "Qtb6boLwIUa2DydMR5Phu3eNjjyNXxuXfay5xHE0",
 
   // Location coordinates for '가톨릭대학교 부천성모병원 장례식장'
-  LATITUDE: 37.487497, // 37.486667 37.483983,
-  LONGITUDE: 126.793038, // 126.790531 126.792178 126.804169,
-  PLACE_NAME: "가톨릭대학교 부천성모병원 장례식장",
+  LATITUDE: 37.487496,
+  LONGITUDE: 126.793034,
+  PLACE_NAME: "부천성모병원 장례식장",
   ADDRESS: "경기도 부천시 소사로 327",
 
   // Kakao Map Place Details / Search page URL
