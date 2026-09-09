6307117c7af0318479b33834892ebd04aa1f1303
Park Shinhee <mam2zyo@gmail.com>
2026-09-10 00:59:40 +0900

    fix: coordinate

--- public/js/config.js
+++ public/js/config.js
@@ -12,7 +12,7 @@ window.INVITATION_CONFIG = {
 
   // Location coordinates for '가톨릭대학교 부천성모병원 장례식장'
   LATITUDE: 37.487350,
-  LONGITUDE: 126.7932200,
+  LONGITUDE: 126.793220,
   PLACE_NAME: "부천성모병원 장례식장",
   ADDRESS: "경기도 부천시 소사로 327",
 
